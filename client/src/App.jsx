15ec9541270381ca6b39be53af3ea1514999d502
Kyle Greenidge <kylegreen149@gmail.com>
2025-09-03 10:50:50 -0400

    Add task component to display each task dynamically

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,30 @@
 import { useState } from 'react'
+import Task from './Task'
 
 function App() {
+const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      name: "Code up my website",
+      urgency: 3
+    },
+    {
+      id: 2,
+      name: "Clean my room",
+      urgency: 1
+    }
+  ])
+
+  // console.log(tasks)
 
   return (
-    <>
-      <h1>Personal To-Do List</h1>
-    </>
+    <div>
+      <h1>My Personal To-Do List</h1>
+      <h2>Add Task That Need to be Completed Here</h2>
+      <ul>
+      {tasks.map(task => <Task key={task.id} name={task.name} urgency={task.urgency}/>)}  
+      </ul>
+    </div>
   )
 }
 
---
+++ client/src/Task.jsx
@@ -0,0 +1,7 @@
+const Task = ({name, urgency}) => {
+    return (
+        <li>{name}, Level of Urgency: {urgency}</li>
+    )
+}
+
+export default Task
