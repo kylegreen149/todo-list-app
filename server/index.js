ac42611c80e3285fbb36610b9625f08180cf8da3
Kyle Greenidge <kylegreen149@gmail.com>
2025-09-06 10:51:37 -0400

    Connect routes to database, setup schemas, install dotenv

--- server/index.js
+++ server/index.js
@@ -1,4 +1,8 @@
+require("dotenv").config() // Load environment variables from a .env file into process.env
+
 const express = require('express') // Used to import express
+const { Task, url } = require("./models/task")
+
 const app = express() // Function that is used to create an Express application stored in the app variable
 
 app.use(express.json()) // Middleware that parses incoming requests with JSON payloads to access data easily
@@ -8,36 +12,41 @@ app.use(express.static('dist')) // Serve static files from the 'dist' directory
 // const cors = require('cors') // Import CORS package to handle cross-origin requests
 // app.use(cors()) // Use CORS middleware to allow cross-origin requests
 
-let tasks = [
-    {
-        id: "1",
-        name: "Make dinner",
-        urgency: 2
-    },
-    {
-        id: "2",
-        name: "Do laundry",
-        urgency: 1
-    },
-    {
-        id: "3",
-        name: "Prepare presentation",
-        urgency: 3
+const mongoose = require("mongoose")
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
+
+const taskSchema = new mongoose.Schema({
+    name: String,
+    urgency: Number,
+})
+
+taskSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
     }
-]
+})
+
+// const Task = mongoose.model('Task', taskSchema)
 
 app.get("/", (req, res) => {
     res.send("<p>Hello World!</>")
 })
 
 app.get("/api/tasks", (req, res) => {
-    res.json(tasks)
+    Task.find({}).then(tasks => {
+        res.json(tasks)
+    })
 })
 
 app.get("/api/tasks/:id", (req, res) => {
     const id = req.params.id
-    const task = tasks.find(t => t.id === id)
-    task ? res.status(200).json(task) : res.status(404).end("Task not found")
+    Task.findById(id).then(task => {
+        task ? res.status(200).json(task) : res.status(404).end("Task not found")
+    })
 })
 
 app.post("/api/tasks", (req, res) => {
@@ -46,20 +55,24 @@ app.post("/api/tasks", (req, res) => {
     if (!body.name || !body.urgency) {
         return res.status(400).json({ error: "name or urgency missing" })
     }
-    const task = {
-        id: (tasks.length + 1).toString(),
+    const task = new Task({
         name: body.name,
         urgency: body.urgency
-    }
-    tasks = tasks.concat(task)
-    res.status(201).json(task)
-    console.log(task)
+    })
+    task.save().then(savedTask => {
+        res.status(201).json(savedTask)
+    })
 })
 
 app.delete("/api/tasks/:id", (req, res) => {
     const id = req.params.id
-    tasks = tasks.filter(t => t.id !== id)
-    res.status(204).end()
+    Task.findByIdAndDelete(id).then(result => {
+        console.log("deleted", result)
+        res.status(204).end()
+    }).catch(error => {
+        console.log(error)
+        res.status(400).send({ error: "malformatted id" })
+    })
 })
 
 // Put this middleware after all request in case all fail, shown if link is invalid
@@ -69,7 +82,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
---
+++ server/models/task.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose")
+
+const password = process.argv[2]
+const url = process.env.MONGODB_URI
+console.log('MongoDB URL:', url)
+
+// Schema used to show how data is stored/ordered in the database
+const taskSchema = new mongoose.Schema({
+    name: String,
+    urgency: Number,
+})
+
+// Change ids from object to string format the time of JSON conversion
+taskSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+})
+
+const Task = mongoose.model('Task', taskSchema)
+
+module.exports = { Task, url }
